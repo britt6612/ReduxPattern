faf6d5c6f4301a7c6e968e5740318f472b49ea24
karatehcops <contact@lazers.tv>
2019-08-06 15:46:50 -0400

    confuse Brittany.

--- src/containers/loginRedux/reducer.js
+++ src/containers/loginRedux/reducer.js
@@ -1,9 +1,12 @@
+// constants.js
 const SET_LOGIN_PENDING = 'SET_LOGIN_PENDING';
 const SET_LOGIN_SUCCESS = 'SET_LOGIN_SUCCESS';
 const SET_LOGIN_ERROR = 'SET_LOGIN_ERROR';
 
-export function login(email, password) {
-  return dispatch => {
+// actions.js
+/*
+export const login = (email, password) =>
+  dispatch => {
     dispatch(setLoginPending(true));
     dispatch(setLoginSuccess(false));
     dispatch(setLoginError(null));
@@ -17,29 +20,65 @@ export function login(email, password) {
       }
     });
   };
-}
+*/
+
+export const callFakeLoginApi = async (email, password) =>
+  setTimeout(() => {
+    if (email === 'admin@admin.com' && password === 'admin') {
+      // mocking up API fetch request
+      return {
+        statusCode: 200,
+        username: 'admin@admin.com',
+        userId: '13193kdwfmwf03'
+      };
+    } else if (email.length === 0 || password.length === 0) {
+      // form validation can be done on form component level.
+      return new Error('Cannot be blank');
+    } else {
+      return new Error('Invalid credentials');
+    }
+  }, 2000);
+
+export const login = async (email, password) => {
+  return async dispatch => {
+    // should be single dispatch
+    dispatch(setLoginPending(true));
+    try {
+      const fakeloginResponse = await callFakeLoginApi('mystuff');
+      if (fakeloginResponse.statusCode === 200) {
+        dispatch(setLoginSuccess(true));
+        // import userActions from '../user/actions
+        // dispatch(userActions.newUser(fakeloginResponse));
+      }
+    } catch (err) {
+      // deal with the error
+      dispatch(setLoginError(err));
+    }
+  };
+};
 
 function setLoginPending(isLoginPending) {
   return {
     type: SET_LOGIN_PENDING,
-    isLoginPending,
+    isLoginPending
   };
 }
 
 function setLoginSuccess(isLoginSuccess) {
   return {
     type: SET_LOGIN_SUCCESS,
-    isLoginSuccess,
+    isLoginSuccess
   };
 }
 
 function setLoginError(loginError) {
   return {
     type: SET_LOGIN_ERROR,
-    loginError,
+    loginError
   };
 }
 
+/* 
 function callFakeLoginApi(email, password, callback) {
   setTimeout(() => {
     if (email === 'admin@admin.com' && password === 'admin') {
@@ -51,30 +90,34 @@ function callFakeLoginApi(email, password, callback) {
     }
   }, 2000);
 }
+*/
 
-export default function reducer(
-  state = {
-    isLoginSuccess: false,
-    isLoginPending: false,
-    loginError: null,
-  },
-  action,
-) {
+// reducer.js
+const initialState = {
+  isLoginSuccess: false,
+  isLoginPending: false,
+  loginError: null
+};
+
+export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_LOGIN_PENDING:
-      return Object.assign({}, state, {
-        isLoginPending: action.isLoginPending,
-      });
+      return {
+        ...initialState,
+        isLoginPending: action.isLoginPending
+      };
 
     case SET_LOGIN_SUCCESS:
-      return Object.assign({}, state, {
-        isLoginSuccess: action.isLoginSuccess,
-      });
+      return {
+        ...initialState,
+        isLoginSuccess: action.isLoginSuccess
+      };
 
     case SET_LOGIN_ERROR:
-      return Object.assign({}, state, {
-        loginError: action.loginError,
-      });
+      return {
+        ...initialState,
+        loginError: action.loginError
+      };
 
     default:
       return state;
